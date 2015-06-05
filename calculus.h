432299ed7f108642b6d1ce8ee5b2999dff0fe246
r0nk <r00nk@simplecpu.com>
2015-06-05 04:13:59 +0000

    removed unused stuff

--- calculus.h
+++
@@ -1,8 +0,0 @@
-#ifndef CALCULUS
-#define CALCULUS
-
-double limit(double approaches);
-double derivative(double (*f)(double x),double x);
-double intergral(double a,double b,double (*f)(double x));
- 
-#endif
